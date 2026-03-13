d6ced6d4bdd1aa129d8daab4b1f0d1c19862a09c
Jsab07 <josesabiop07@gmail.com>
2026-03-13 12:48:00 +0100

    fix: problemas

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/dominio/Cliente.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.tallermecanico.dominio;
+
+public class Cliente {
+    public static final String ER_NOMBRE = '/d{9}';
+    public static final String ER_DNI = '((\\d{8})(\\D))';
+    public static final String ER_TELEFONO = '';
+
+
+}
